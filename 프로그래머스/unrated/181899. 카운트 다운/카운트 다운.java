f2f884090dfccc3e54be2ece7f88efe777f6263f
제보민 <42407430+jebomin@users.noreply.github.com>
2023-09-18 17:35:20 +0900

    [unrated] Title: 카운트 다운, Time: 0.02 ms, Memory: 73.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181899. 카운트 다운/카운트 다운.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] solution(int start, int end_num) {
+        int size = start-end_num+1;
+        int[] answer = new int[size];
+        
+        for(int i = 0; i < size; i++){
+            answer[i] = start--;
+        }
+        return answer;
+    }
+}
